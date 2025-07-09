f77e8ee751bf02fef2078e3f00d8fecceed3233c
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-09 20:04:32 +0530

    added the vite.config

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.jsx'),
+      name: 'OpenIMISFeTools',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`
+    },
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        /^@babel.*/,
+        /^@date-io\/.*/,
+        /^@material-ui\/.*/,
+        /^@openimis.*/,
+        'classnames',
+        'clsx',
+        'history',
+        /^lodash.*/,
+        'moment',
+        'prop-types',
+        /^react.*/,
+        /^redux.*/
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
