7ebe7161438dce74e61c344949f4f2928759140f
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-29 20:56:18 +0530

    update package.josn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  plugins: [react({
+    jsxRuntime: 'automatic',
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.jsx'),
